d322abded4cb092c1fab183c7dc3145bb4faea23
superiorshrimp <szymon.zychowicz300701@gmail.com>
2021-12-02 11:06:11 +0100

    21aoc2

--- 2021/1/main.py
+++ 2021/1/main.py
@@ -1,20 +1,19 @@
-def ex1(file):
-    with open(file, "r") as file:
-        lines = [int(row) for row in file]
+def ex1(lines):
     counter = 0
     for i in range(1,len(lines)):
         if lines[i]>lines[i-1]:
             counter+=1
     return counter
 
-def ex2(file):
-    with open(file, "r") as file:
-        lines = [int(row) for row in file]
+def ex2(lines):
     counter = 0
     for i in range(len(lines)-3):
         if lines[i+3] > lines[i]:
             counter +=1
     return counter
 
-print(ex1('./1/data.txt'))
-print(ex2('./1/data.txt'))
+with open('./1/data.txt', "r") as f:
+    lines = [int(row) for row in f]
+
+print(ex1(lines))
+print(ex2(lines))
---
+++ 2021/2/main.py
@@ -0,0 +1,29 @@
+def ex1(lines):
+    depth, horizontal = 0, 0
+    for i in range(len(lines)):
+        if lines[i][0][0] == 'f':
+            horizontal += int(lines[i][1])
+        elif lines[i][0][0] == 'd':
+            depth += int(lines[i][1])
+        else:
+            depth -= int(lines[i][1])
+    return depth*horizontal
+
+def ex2(lines):
+    horizontal, depth, aim = 0, 0, 0
+    for i in range(len(lines)):
+        if lines[i][0][0] == 'f':
+            horizontal += int(lines[i][1])
+            depth += aim*int(lines[i][1])
+        elif lines[i][0][0] == 'd':
+            aim += int(lines[i][1])
+        else:
+            aim -= int(lines[i][1])
+    return depth*horizontal
+
+    
+with open('./2/data.txt', 'r') as f:
+    lines = [row.split() for row in f]
+
+print(ex1(lines))
+print(ex2(lines))
